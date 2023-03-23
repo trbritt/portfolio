0d64a715d6b0df150f7b9188694e2d8c809aa83b
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-03-23 19:17:21 -0400

    corrected navbar rooting on click

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20  backdrop-blur-sm backgrop-filter shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]`}>
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
-        <Link to="/" className='flex items-center gap-2' onClick={()=> {setActive("");window.scrollTo(0,0);}}>
+        <Link to="/portfolio" className='flex items-center gap-2' onClick={()=> {setActive("");window.scrollTo(0,0);}}>
           <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
           <p className='test-white test-[18px] font-bold cursor-pointer flex'>
             Tristan&nbsp;
