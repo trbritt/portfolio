4a6d6ef5a24a9a412db9e513412120e83808073d
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-04-25 20:28:37 -0400

    fixed placement of popup on reg devs

--- src/components/Dissertation.jsx
+++ src/components/Dissertation.jsx
@@ -58,7 +58,7 @@ export const Dissertation = ({ isOpen, setIsOpen }) => {
 							</span>
 
 							<div
-								className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-center sm:max-w-lg sm:w-full"
+								className="inline-block align-center bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-center sm:max-w-lg sm:w-full"
 								role="dialog"
 								aria-modal="true"
 								aria-labelledby="modal-headline"
