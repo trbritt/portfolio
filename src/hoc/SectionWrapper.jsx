af80e6b5d8299a4a377bfa3f95d5dabba0e1dec6
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-04-25 20:21:43 -0400

    just don't hide first text on mobile

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -6,18 +6,19 @@ import { staggerContainer } from "../utils/motion";
 const SectionWrapper = (Component, idName) =>
   function HOC() {
     return (
-      <>
-      <span className='hash-span' id={idName}/>
-      <motion.section
-        variants={staggerContainer()}
-        initial='hidden'
-        whileInView='show'
-        viewport={{ once: true, amount: 0.25 }}
-        className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
-      >
+      <>        
+        <span className='hash-span' id={idName}/>
+        <motion.section
+          variants={staggerContainer()}
+          initial={idName === 'about' ? 'show' : 'hidden'}
+          whileInView='show'
+          viewport={{ once: true, amount: 0.25 }}
+          className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+        >
 
-        <Component />
-      </motion.section>
+
+          <Component />
+        </motion.section>
       </>
     );
   };
