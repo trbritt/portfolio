3b888f637d3672a88669d9a022d44c80dbc04e58
trbritt <trbritt@users.noreply.github.com>
2024-11-24 22:43:55 +0000

    deploy: 6fe3e697a5e05f9fa8faf65e89f380e6610929ab

---
+++ _next/static/chunks/72167.24f82e65cecf37cb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[72167],{72167:function(e,c,r){r.r(c),c.default={connecting:"Conectando a Smart Account",failedToConnect:"Error al conectar con Smart Account",wrongNetworkScreen:{title:"Red incorrecta",subtitle:"Tu cartera no est\xe1 conectada a la red requerida",failedToSwitch:"Error al cambiar de red"}}}}]);
